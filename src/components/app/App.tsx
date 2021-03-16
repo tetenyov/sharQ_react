4ce66ed02d9d5f3258e809119f60e7852bca5e4d
vovo <tetenev@yandex.ru>
2021-03-16 22:41:49 +0300

    inserted main bg image for App component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import bgImage from '../../img/bg_upper_part.png'
+
 function App() {
   return (
-    <>
+    <div className='page-wrapper' style={{backgroundImage: `url(${bgImage})`}}>
       <h1>Sharq</h1>
-    </>
+    </div>
   );
 }
 
