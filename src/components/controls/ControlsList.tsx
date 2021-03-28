70dffd61a073ffc6995920c4988d208ddc67c41c
vovo <tetenev@yandex.ru>
2021-03-28 13:30:05 +0300

    Problem >> aligned cells and set sizes

--- src/components/controls/ControlsList.tsx
+++ src/components/controls/ControlsList.tsx
@@ -9,7 +9,7 @@ function ControlsList() {
     const controlValue = (NUMBER_CONTROLS - 1) - i
 
     return (
-      <li className="controls-list__item">
+      <li className="controls-list__item" key={i}>
         <Control type="button" value={controlValue}>{ controlValue }</Control>
       </li>
     )
@@ -18,10 +18,10 @@ function ControlsList() {
   return (
     <ul className="controls-list">
       { digitControls }
-      <li className="controls-list__item controls-list__item--minus">
+      <li className="controls-list__item controls-list__item--minus" key={'11'}>
         <Control type="button" value="-">&#8722;</Control>
       </li>
-      <li className="controls-list__item controls-list__item--delete">
+      <li className="controls-list__item controls-list__item--delete" key={'12'}>
         <Control type="button">&#8592;</Control>
       </li>
     </ul>
