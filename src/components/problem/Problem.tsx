565c141f96e03503cd2aacab328688c1f28f7a95
vovo <tetenev@yandex.ru>
2021-04-02 09:05:58 +0300

    typed problemReducer and linked promlem fields to store

--- src/components/problem/Problem.tsx
+++ src/components/problem/Problem.tsx
@@ -2,8 +2,8 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 
 function Problem() {
-  const problem = useSelector((state: RootState) => state.problem.problem)
-
+  const problem = useSelector((state: RootState) => state.problem)
+  console.log(problem)
   return (
     <section className='problem'>
       <h2 className='visually-hidden'>Problem</h2>
@@ -12,13 +12,19 @@ function Problem() {
           <tr className='problem__answer'>
             <td className='problem__cell problem__cell--answer'>
               <input className='problem__answer-input' type='text' id='answer'
-                value={problem} disabled />
+                disabled />
             </td>
           </tr>
           <tr className='problem__question'>
-            <td className='problem__cell problem__cell--first-integer' />
-            <td className='problem__cell problem__cell--operation' />
-            <td className='problem__cell problem__cell--second-integer' />
+            <td className='problem__cell problem__cell--first-integer'>
+              {problem.leftInt}
+            </td>
+            <td className='problem__cell problem__cell--operation'>
+              {problem.operation}
+            </td>
+            <td className='problem__cell problem__cell--second-integer'>
+              {problem.rightInt}
+            </td>
           </tr>
         </tbody>
       </table>
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import { rootReducer } from './reducers'
-import sagaWatcher from './middlewares'
+import sagaWatcher from './middlewares/saga'
 
 const sagaMiddleware = createSagaMiddleware()
 
