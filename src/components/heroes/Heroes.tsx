4ffa7b1f99b78a0103acc7bea2025dc579f1077f
vovo <tetenev@yandex.ru>
2021-04-07 08:07:34 +0300

    placed heroes on the scene

--- src/components/heroes/Heroes.tsx
+++ src/components/heroes/Heroes.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-
-import bgImage from '../../img/bg_upper_part.png'
+import bgImage from '../../img/bg_upper_part.png';
+import ducky from '../../img/ducky.webp'
+import shark from '../../img/shark.webp'
 
 function Heroes() {
   return (
     <section className='heroes' style={{ backgroundImage: `url(${bgImage})` }}>
-       <h2 className="visually-hidden">Поле с анимацией</h2>
-      
+      <h2 className="visually-hidden">Поле с анимацией</h2>
+      <img className='heroes__ducky' src={ducky} alt='yellow duck'/>
+      <img className='heroes__shark' src={shark} alt='angry shark'/>
     </section>
-  )
-}
+  );
+};
 
-export default Heroes
+export default Heroes;
