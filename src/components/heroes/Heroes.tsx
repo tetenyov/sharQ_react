bfd7541e56a3cf7770dab1c700a22a101179874d
vovo <tetenev@yandex.ru>
2021-04-10 22:05:12 +0300

    handle finish round by ducks loose

--- src/components/heroes/Heroes.tsx
+++ src/components/heroes/Heroes.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { RootState } from '../../store/index';
@@ -7,8 +7,13 @@ import { finishRound } from '../../store/action-creators/action-creators';
 import bgImage from '../../img/bg_upper_part.png';
 import ducky from '../../img/ducky.webp';
 import shark from '../../img/shark.webp';
+import angryShark from '../../img/angry_shark.webp';
 
 function Heroes() {
+  const [ isAngry, setIsAngry ] = useState(false);
+  const [ isDucky, setIsDucky ] = useState(true);
+  const [ isAnimation, setIsAnimation ] = useState(false);
+
   const isStart = useSelector((state: RootState) => state.progress.isStart);
   const position = useSelector((state: RootState) => state.progress.position);
   const sharkSpeed = useSelector((state: RootState) => state.settings.speed);
@@ -23,7 +28,15 @@ function Heroes() {
     const shark = sharkRef.current?.getBoundingClientRect().right;
 
     if (shark && ducky) {
-      if (shark > ducky) {
+
+      if (ducky - shark < 15 && ducky - shark > 0) {
+        setIsAngry(true)
+      }
+
+      if (shark - ducky > 25) {
+        setIsAngry(false);
+        setIsAnimation(false);
+        setIsDucky(false);
         dispatch(finishRound());
       }
     }
@@ -34,7 +47,10 @@ function Heroes() {
 
     if (isStart) {
       interval = setInterval(handleHeroesPositions, 200)
+      setIsAnimation(true)
     }
+
+    setIsDucky(true);
 
     return () => clearInterval(interval);
   }, [isStart]);
@@ -42,12 +58,13 @@ function Heroes() {
   return (
     <section className='heroes' style={{ backgroundImage: `url(${bgImage})` }}>
       <h2 className='visually-hidden'>Поле с анимацией</h2>
-      <img className='heroes__ducky' src={ducky} alt='yellow duck'
+      <img className='heroes__ducky' src={isDucky ? ducky : ''} alt='yellow duck'
         ref={duckyRef} style={{left: `${position}vw`}}
       />
       <img className={`${isStart ? 'heroes__shark heroes__shark--move' : 'heroes__shark'}`} 
-        src={shark} alt='angry shark'
-        style={{animationDuration: `${25 / sharkSpeed }s`}} ref={sharkRef}
+        src={isAngry ? angryShark : shark} alt='angry shark'
+        style={{animationDuration: `${25 / sharkSpeed }s`, animationPlayState: isAnimation ? 'running' : 'paused'}} 
+        ref={sharkRef}
       />
     </section>
   );
